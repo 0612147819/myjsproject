3bfc65406143eb30b9cacd9ab9a7aaae0ca200f4
Tawatchai Pitak <jokjokjok00720@gmail.com>
2024-06-28 11:54:48 +0700

    Update sum.test.js
    update sum2,3

--- sum.test.js
+++ sum.test.js
@@ -3,3 +3,15 @@ const sum = require('./sum')
 test('Adds 1 + 2 to equal 3', () => {
     expect(sum(1,2)).toBe(3);
 })
+
+const sum2 = require('./sum')
+
+test('Adds 4 + 5 to equal 9', () => {
+    expect(sum(4,5)).toBe(9);
+})
+
+const sum3 = require('./sum')
+
+test('Adds 4 + 3 to equal 7', () => {
+    expect(sum(4,3)).toBe(7);
+})
